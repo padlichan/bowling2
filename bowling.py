d1a85e2dc7731b3b2063eacdf861ac8a27233e0e
padlichan <lili.berenyi@gmail.com>
2025-06-11 17:02:02 +0100

    add and pass tests for single strike and single open frame

--- bowling.py
+++ bowling.py
@@ -1,11 +1,7 @@
-def calc_score(rounds):
-    total_score = 0
-    rounds = rounds.replace(" ", "")
-    for i in range(0, len(rounds)):
-        if rounds[i] == "X":
-            total_score += 10
-        elif rounds[i] == "/":
-            total_score += (10 - int(rounds[i-1]))
-        else:
-            total_score += int(rounds[i])
-    return total_score
+def calc_score(frames):
+   if frames == "X":
+        return 10
+   elif len(frames) == 2:
+       return int(frames[0]) + int(frames[1])
+   else: 
+        return 0
--- bowling_test.py
+++ bowling_test.py
@@ -2,11 +2,11 @@ import unittest
 from bowling import calc_score
 
 class TestCalcScore(unittest.TestCase):
-    def test_does_simple_numbers(self):
-        self.assertEqual(calc_score("11 11 11 11"), 8)
+    def test_does_one_empty(self):
+        self.assertEqual(calc_score(""), 0)
 
-    def test_does_X(self):
-        self.assertEqual(calc_score("X 11 54 81"), 30)
-    
-    def test_does_slash(self):
-        self.assertEqual(calc_score("4/ 11 11 X"), 24)
+    def test_does_one_strike(self):
+        self.assertEqual(calc_score("X"), 10)
+
+    def test_does_one_open_frame(self):
+        self.assertEqual(calc_score("23"), 5)
