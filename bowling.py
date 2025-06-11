20cc1163b7af60aea0eec077fa73487533c84d22
Ivo-velev <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>
2025-06-11 13:54:47 +0100

    added test for X

--- bowling.py
+++ bowling.py
@@ -2,5 +2,8 @@ def calc_score(rounds):
     total_score = 0
     rounds = rounds.replace(" ", "")
     for score in rounds:
-        total_score += int(score)
+        if score != "X":
+            total_score += int(score)
+        else:
+            total_score += 10
     return total_score
--- bowling_test.py
+++ bowling_test.py
@@ -5,3 +5,5 @@ class TestCalcScore(unittest.TestCase):
     def test_does_simple_numbers(self):
         self.assertEqual(calc_score("11 11 11 11"), 8)
 
+    def test_does_X(self):
+        self.assertEqual(calc_score("X 11 54 81"), 30)
